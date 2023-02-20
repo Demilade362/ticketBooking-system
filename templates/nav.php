ef458809c811ec692003581f0fae54946befcf79
Demilade362 <ademolademilade362@gmail.com>
2023-02-20 12:25:22 +0100

    Remove Center button from the navbar

--- templates/nav.php
+++ templates/nav.php
@@ -40,16 +40,6 @@
                         </ul>
                     </li>
                 <?php endif; ?>
-                <div class="dropdown-center">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Centered dropdown
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Action two</a></li>
-                        <li><a class="dropdown-item" href="#">Action three</a></li>
-                    </ul>
-                </div>
 
                 <?php if (isset($_SESSION['username'])) : ?>
                     <li class="nav-item">
